968d10541e55c9b00ddede30887166f280dd2e77
pidtuner <pidtuner@pidtuner.com>
2021-11-05 18:36:59 +0100

    Updated dependencies to most recent (working) versions

--- sw.js
+++ sw.js
@@ -1,11 +1,9 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.0/workbox-sw.js');
 
 if (workbox) { 
-    // force dev build until https://github.com/GoogleChrome/workbox/issues/2686 gets fixed
-    //workbox.setConfig({ debug: true });
     // caching strategy
     workbox.precaching.precacheAndRoute([
-        { url: 'index.html'                    , revision: '1.0.6.0' },
+        { url: 'index.html'                    , revision: '1.0.6.1' },
         { url: 'pid/v1.0.6/pid_tuner.js'       , revision: '1.0.6.0' },
         { url: 'pid/v1.0.6/pid_tuner_wasm.wasm', revision: '1.0.6'   }
     ]);
@@ -14,11 +12,11 @@ if (workbox) {
         new workbox.strategies.StaleWhileRevalidate()
     );
     workbox.routing.registerRoute(
-        new RegExp('https:\/\/cdn\.jsdelivr\.net.*min\.(css|js)'),
+        new RegExp('https:\/\/cdn\.jsdelivr\.net.*\.(css|js)'),
         new workbox.strategies.CacheFirst()
     );
     workbox.routing.registerRoute(
-        new RegExp('https:\/\/cdnjs\.cloudflare\.com.*min\.(css|js)'),
+        new RegExp('https:\/\/cdnjs\.cloudflare\.com.*\.(css|js)'),
         new workbox.strategies.CacheFirst()
     );
 }
